d1efad8714ad0ad087abaf7357fb031fbd3a9de0
umarahgul <umarah.gul@gmail.com>
2023-09-08 01:42:56 +0500

    added styling to the page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import displayScores from './modules/displayScores';
 import { createGame, submitScore, getScores } from './modules/consumeApi';
+//import resident.jpg from './src/assets';
 
 const populate = async () => {
   const gameId = localStorage.getItem('gameId');
